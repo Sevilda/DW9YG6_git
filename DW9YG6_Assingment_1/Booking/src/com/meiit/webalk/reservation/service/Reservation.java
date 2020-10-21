ae8059f9a3e2fcc857464ba7ea02ed839023b693
Struti <struti.alex69@gmail.com>
2020-10-21 18:52:11 +0200

    Update Reservation.java

--- DW9YG6_Assingment_1/Booking/src/com/meiit/webalk/reservation/service/Reservation.java
+++ DW9YG6_Assingment_1/Booking/src/com/meiit/webalk/reservation/service/Reservation.java
@@ -8,19 +8,28 @@ import com.meiit.webalk.reservation.domain.BookingPerson;
 import com.meiit.webalk.reservation.domain.Currency;
 import com.meiit.webalk.reservation.domain.Hotel;
 
+// This is not a correct interface
+// Dont modify the provided interface, Use the provided nameing!!
 public interface Reservation {
-	
+	//Do not use statefull interface!
+	//The expectation is not bad but this is should be in the implementation
+	------------
 	Reservation res=null;
 	static ArrayList<Hotel> hotels=new ArrayList<Hotel>();
 	static ArrayList<Reservation> reservations=new ArrayList<Reservation>();
+	----------------
 	
+	//Interface is not contains epmty methods only the method signature!
+	//static is not part of the original interface
 	public static void saveBookingPerson (BookingPerson bp) {}
 	
+	//Never put implementation in the interface
 	public static BookingPerson findBookingPerson() {
 		BookingPerson bp=null;
 		return bp;
 	}
 	
+	//Based on the doc you need to use List
 	public static ArrayList<Hotel> findAllHotels () {
 		return hotels;
 	}
@@ -29,6 +38,7 @@ public interface Reservation {
 		reservations.add(res);
 	}
 	
+	//same
 	public static ArrayList<Reservation> findAllReservations () {
 		return reservations;
 	}
