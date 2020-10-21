d88c11e83115220f85b359f84acfc790c2b0aedf
Struti <struti.alex69@gmail.com>
2020-10-21 22:32:24 +0200

    Review

--- DW9YG6_Assingment_1/Booking/src/com/meiit/webalk/reservation/service/ReservationService.java
+++ DW9YG6_Assingment_1/Booking/src/com/meiit/webalk/reservation/service/ReservationService.java
@@ -12,17 +12,28 @@ import com.meiit.webalk.reservation.domain.Room;
 
 public class ReservationService implements Reservation {
 	
-	private Status status; //booked, active, processed - don't see the point in using booleans when we can use enums for the same purpose. 
+	//Any way this is also a useless declaration....
+	-------------------
+	private Status status; 
+	//booked, active, processed - don't see the point in using booleans when we can use enums for the same purpose. 
+	//Why are you use enum when the actual status can defined in 2 state,
+	//Ask before you do a similar comments like this.
 	private Hotel hotel;
 	private Room room;
 	private Date fromDate = new Date();
 	private Date toDate = fromDate;
 	private Currency curr;
+	//BigDecimal requvired...  
 	private int amount;
 	private Reservation res;
+	----------------------
+	//Ask, and read documentation before you make arrogant comments!!
+	
 	private ArrayList<Hotel> hotels=new ArrayList<Hotel>();
 	private ArrayList<Reservation> reservations=new ArrayList<Reservation>();
 	
+	//Constructor not need to the solution
+	-----------------
 	public ReservationService(Hotel h, Currency c, int a, Room R) {
 		hotel=h;
 		curr=c;
@@ -31,15 +42,28 @@ public class ReservationService implements Reservation {
 	}
 	
 	public ReservationService() {}
+	--------------------
 	
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public static Status getStatus(ReservationService res) {
 		return res.status;
 	}
 
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public static void setStatus(Status status, ReservationService res) {
 		res.status = status;
 	}
 
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public Date getFromDate() {
 		Calendar fromCal = Calendar.getInstance();
 		fromCal.setTime(fromDate);
@@ -48,45 +72,78 @@ public class ReservationService implements Reservation {
 		//return fromDate;
 	}
 
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public void setFromDate(Date fromDate) {
 		this.fromDate = fromDate;
 	}
 
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public Date getToDate(ReservationService res) {
 		return res.toDate;
 	}
 
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public void setToDate(Date toDate, ReservationService res) {
 		res.toDate = toDate;
 	}
 
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public static Currency getCurr(ReservationService res) {
 		return res.curr;
 	}
 
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public static void setCurr(Currency curr, ReservationService res) {
 		res.curr = curr;
 	}
 	
-	
+	//Almost 
 	public static void checkIn (ReservationService res) {
 		res.status=Status.ACTIVE;
 	}
-	
+	//Almost 
 	public static void checkOut (ReservationService res) {
 		res.status=Status.PROCESSED;
 	}
 	
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public String toString() {
 		SimpleDateFormat sdt = new SimpleDateFormat("yyyy-MM-dd");
 		return "Your reservation has been set for " + sdt.format(fromDate) + " at the Hotel " + hotel.getName() + ", in room " + room.getNumber(room);
 	}
 
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public Room getRoom(ReservationService res) {
 		return res.room;
 	}
 
+	//Use meaningfull variables res not accaptable
+	//If is not part of interface method should be private
+	//Useless method not need for the solution
+	//You violate KISS and YAGNI
 	public void setRoom(Room room, ReservationService res) {
 		res.room = room;
 	}
 }
+//For All service getter and setter methods should not live in service all solution is WRONG!!
