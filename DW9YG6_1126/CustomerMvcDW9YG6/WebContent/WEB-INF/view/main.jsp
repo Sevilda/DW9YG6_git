94a248dd5199d08846fd05c4cb2d428f7f7e29f9
Sevilda <silversapphire7@gmail.com>
2020-11-26 11:44:05 +0100

    Update

---
+++ DW9YG6_1126/CustomerMvcDW9YG6/WebContent/WEB-INF/view/main.jsp
@@ -0,0 +1,8 @@
+<html>
+<head></head>
+<body>
+
+	<h1>Main Page DW9YG6</h1>
+
+</body>
+</html>
---
+++ DW9YG6_SemTask/SemTaskDW9YG6/src/test/java/hu/dw9yg6/semtask/SemTaskDW9YG6/SemTaskDw9Yg6ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.dw9yg6.semtask.SemTaskDW9YG6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemTaskDw9Yg6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
